adb197e41f28fa48fe916ab49a20f4c4c2ee7fc5
bmcmahen <ben.mcmahen@gmail.com>
2013-11-13 12:39:43 -0700

    add slower, but more flexible method; needs to be optimized

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ function isMatch(node, selector){
 module.exports = function(selector){
   // Good browsers
   if (window.getSelection) {
+    
     var selection = window.getSelection();
     var range = selection.getRangeAt(0);
-    var start = range.startContainer;
-    var end = range.endContainer;
-
-    var current = start;
+    var all = range.commonAncestorContainer.getElementsByTagName('*');
     var nodes = [];
 
-    while (current != end) {
-      if (selector){
-        if (isMatch(current, selector)){
-          nodes.push(current);
+    for (var i = 0, len = all.length; i < len; i++) {
+      var el = all[i];
+      if (selection.containsNode(el, true)){
+        if (selector) {
+          if (isMatch(el, selector)) {
+            nodes.push(el);
+          }
+        } else {
+          nodes.push(el); 
         }
-      } else {
-        nodes.push(current);
       }
-      current = current.nextSibling;
-    };
+    }
 
     return nodes;
 
